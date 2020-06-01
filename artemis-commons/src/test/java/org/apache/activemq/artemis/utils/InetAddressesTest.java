69bd10667c38ad5be57dd6e30f1b883765282d9d
a. p. matthews <i@apmatthe.ws>
2020-05-31 20:53:43 -0700

    fix some style concerns

--- artemis-commons/src/test/java/org/apache/activemq/artemis/utils/InetAddressesTest.java
+++ artemis-commons/src/test/java/org/apache/activemq/artemis/utils/InetAddressesTest.java
@@ -12,15 +12,15 @@ import org.apache.activemq.artemis.utils.InetAddresses;
 public class InetAddressesTest {
    @Test
    public void testForUriString() throws Throwable {
-	  // check IPv4 address
-	  Inet4Address addr4 = (Inet4Address)InetAddresses.forUriString("127.0.0.1");
-	  Assert.assertEquals(addr4.getHostAddress(), "127.0.0.1");
+      // check IPv4 address
+      Inet4Address addr4 = (Inet4Address)InetAddresses.forUriString("127.0.0.1");
+      Assert.assertEquals(addr4.getHostAddress(), "127.0.0.1");
 
-	  // check IPv6 address
-	  Inet6Address addr6 = (Inet6Address)InetAddresses
-	     .forUriString("[0:1111:2222:3333:4444:5555:6666:7777]");
-	  Assert.assertEquals(addr6.getHostAddress(),
-	     "0:1111:2222:3333:4444:5555:6666:7777");
+      // check IPv6 address
+      Inet6Address addr6 = (Inet6Address)InetAddresses
+         .forUriString("[0:1111:2222:3333:4444:5555:6666:7777]");
+      Assert.assertEquals(addr6.getHostAddress(),
+         "0:1111:2222:3333:4444:5555:6666:7777");
    }
 
    @Test
